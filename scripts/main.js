817cf3624b4a401b2f8bb2809b781c7d35d05cf7
sasanksriram <sasank.sriram.2012@gmail.com>
2016-08-15 00:46:30 +0530

    Included jQuery, changed some file names

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	var pLeftDD1 = parseInt($('#m1').position().left) - 200; /*200 is random, so that it comes to the center*/
+	$('#ddM1').css('padding-left', pLeftDD1);
+
+	var pLeftDD3 = parseInt($('#m3').position().left) - 200; /*For Menu3*/
+	$('#ddM3').css('padding-left', pLeftDD3);
+	
+});
--- scripts/sampark_na.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-	/*Making the dropdown come under the menu, getBoundingClientRect().left is so that the dropdown aligns,
-	 200 is random so that it comes approxiamately to center */
-	document.getElementById('ddM1').style.paddingLeft = document.getElementById('m1').getBoundingClientRect().left - 200 + "px"; 
-	document.getElementById('ddM3').style.paddingLeft = document.getElementById('m3').getBoundingClientRect().left - 200 + "px"; 
-	/* For Menu 3 */
-}
